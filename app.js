c817fc005527a94498224d4b43f16a4eb254e000
Akshat kumar <akshatrai38@gmailcom>
2024-07-26 23:59:38 +0530

    ADD Project Files

---
+++ app.js
@@ -0,0 +1,156 @@
+if(process.env.NODE_ENV != "production") {
+  require('dotenv').config();
+}
+
+
+
+
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+// const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+// const wrapAsync = require("./utils/wrapAsync.js");
+ //const ExpressError = require("./utils/ExpressError.js");
+// const { listingSchema ,reviewSchema } = require("./schema.js");
+// const Review = require("./models/review.js");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const user = require("./models/user.js");
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+
+
+ const dbUrl = process.env.ATLASDB_URL;
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) =>{
+
+    console.log(err);
+  });
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.engine("ejs" , ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error" , ()=> {
+  console.log("ERROR in MONGO SESSION STORE" , err);
+});
+
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly : true,
+  },
+};
+
+
+// app.get("/", (req,res) => {
+//   res.send("Hi , I am root");
+// });
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(user.authenticate()));
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
+
+app.use((req , res , next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  
+
+  next();
+
+  });
+
+  // app.get("/demouser" , async(req,res) => {
+  //   let fakeUser = new user({
+  //     email: "student@gmail.com",
+  //     username: "delta-student",
+  //   });
+
+  //   let registeredUser = await user.register(fakeUser , "helloworld");
+  //   res.send(registeredUser);
+  // });
+
+
+
+
+ 
+ app.use("/listings" , listingRouter);
+ app.use("/listings/:id/reviews" , reviewRouter);
+ app.use("/" , userRouter);
+
+
+ 
+
+//  app.get("/testListing", async (req , res) => {
+//   let sampleListing = new Listing({
+//       title: "My New Villa",
+//       description: "By the beach",
+//       price: 1200,
+//       location: "Calangute,Goa" ,
+//       country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+app.all("*" , (req , res , next) => {
+  next(new ExpressError(404 , "Page Not found!"));
+});
+
+app.use((err,req,res,next) => {
+  let {statusCode=500, message="Something went wrong!"} = err;
+  res.render("error.ejs" , {err});
+// res.status(statusCode).send(message);
+});
+
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
